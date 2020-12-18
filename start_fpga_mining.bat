6df18f5b07b8f93b2b41610f6f27bf92b7957e9f
Mahmoud Khaled <khaled.mahmoud@tum.de>
2020-12-18 08:54:18 +0100

    mods

---
+++ start_fpga_mining.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set PYTHONPATH=.\
+
+set ACCOUNT=mkhaled87
+set ACCOUNT_PASSWORD=21235365876986800
+set WORKER=FPGAs
+set MINING_POOL=btc-eu.f2pool.com
+set POOL_PORT=3333
+
+set TARGET_PLATFORM_ID=0
+set TARGET_DEVICE_ID=0
+set LOCAL_WG_SIZE=256
+SET NUM_CUS=8
+set BINARY_FILE="D:\myWorkspace\GitLab\clypse_fpga\apoclypsebm\bin\s10_sh2e1_4Gx2_cu%NUM_CUS%\apoclypse_lws%LOCAL_WG_SIZE%.aocx"
+
+cls
+@echo Starting the BTC miner with CU=%NUM_CUS%/LWS=%LOCAL_WG_SIZE%
+python .\apoclypsebm\command.py  -p %TARGET_PLATFORM_ID% -d %TARGET_DEVICE_ID% -i %BINARY_FILE% -w %LOCAL_WG_SIZE% --verbose stratum+tcp://%ACCOUNT%.%WORKER%:%ACCOUNT_PASSWORD%@%MINING_POOL%:%POOL_PORT%
